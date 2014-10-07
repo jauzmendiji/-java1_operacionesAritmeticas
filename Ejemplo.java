1a23a627d0165922335d0bb00c7ad21b3f9a61c2
zubiri <zubiri@Ubuntu-12-04.(none)>
2014-10-07 08:40:23 +0200

    Ariketa aritmetikoak

---
+++ Ejemplo.java
@@ -0,0 +1,14 @@
+public class Ejemplo{
+	public static void main(String args[]){
+		int edadLuis=24;
+		if(edadLuis>=18){
+			System.out.println("Luis es mayor de edad y puede votar");
+			System.out.println("Luis tiene "+edadLuis+" a"+(char)164+"os");
+		}
+		else{
+			System.out.println("Luis no es mayor de edad y no puede votar");
+			System.out.println("Luis tiene "+edadLuis+" a"+(char)164+"os");
+		}
+		System.out.println("FIN DE PROGRAMA");
+	}
+}
